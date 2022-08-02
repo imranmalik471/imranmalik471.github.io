0f26af04c6965825d3a3f8e69bc50da7be562ac9
Imran Malik <imran.malik@cartlow.com>
2022-08-02 11:00:09 +0500

    New components Added

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,30 +8,33 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            Hi Everyone, I am <span className="purple">Front End Developer </span>
+            from <span className="purple"> Pakistan.</span>
+            <br />I am a Computer Science Graduate from Government College University, Lahore. I have three years pf experience in Front end development.
+            I enjoys the problem solving and coding. Always strive to learn new things and give my 100%. I can make website for your business to grow up your sales.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            I have expertise in the following domains:
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Custom Web Development
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writting Tech Blogs
+              <ImPointRight /> WordPress Customization
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> PSD to HTML, Figma to HTML, XD to HTML
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> CMS/CRM Development
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Imran Malik</footer>
         </blockquote>
       </Card.Body>
     </Card>
